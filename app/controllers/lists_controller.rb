daa8d28372aaefef0c6045be0eed4726639da58c
kyousuke678 <119562080+kyousuke678@users.noreply.github.com>
2022-12-05 07:28:05 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+
+  def new
+    @list = List.new
+  end
+
+  # 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+  
+  def index
+    @lists = List.all  
+  end
+  
+  def show
+    @list = List.find(params[:id])  
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)  
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
