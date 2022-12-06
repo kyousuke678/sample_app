129d2fa273f1393baf0d2b924df84537e15acaaf
kyousuke678 <119562080+kyousuke678@users.noreply.github.com>
2022-12-06 05:44:33 +0000

    Finish-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,6 +30,16 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)  
   end
   
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
   private
   # ストロングパラメータ
   def list_params
